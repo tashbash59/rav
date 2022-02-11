ec6caff8022a8a90fe5d1bfc39e3fd19f9c0ff12
tashbash59 <tashbaev300@gmail.com>
2022-02-11 16:19:27 +0500

    f

---
+++ glavnaya/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings 
+from django.utils import timezone 
+
+class Post(models.Model):
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	published_date = models.DateTimeField(blank=True,null=True)
+
+	def publish(self):
+		self.save()
+
+	def __str__(self):
+		return self.title
+
